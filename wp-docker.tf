1cf05ea7d77929cc7c7ea028be0d7d0934b823fe
sunny <sunny@pop-os.localdomain>
2021-08-16 18:59:33 +1000

    V1 Ubuntu server in GCP - Dockerized WP [1 DB and 1 WP docker container]

---
+++ wp-docker.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  region  = "us-central1"
+  project = "skilled-eon-323007"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  metadata_startup_script = "${file("~/projects/cryptomoonlamp/scripts/bootstrap.sh")}"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20210720"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
